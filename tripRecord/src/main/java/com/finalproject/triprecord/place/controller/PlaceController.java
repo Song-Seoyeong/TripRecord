43c4bef5ddef09688c5f2a6eee8d043aa9bbe88d
seoyeong <tjdud654@naver.com>
2024-07-23 02:04:43 +0900

    .

---
+++ tripRecord/src/main/java/com/finalproject/triprecord/place/controller/PlaceController.java
@@ -0,0 +1,28 @@
+package com.finalproject.triprecord.place.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class PlaceController {
+	
+	@GetMapping("recoPlace.pla")
+	public String recoPlacelist() {
+		return "recommendPlace";
+	}
+	
+	@GetMapping("placeDetail.pla")
+	public String placeDetail() {
+		return "placeDetail";
+	}
+	
+	@GetMapping("placeReviewWrite.pla")
+	public String placeReviewWrite() {
+		return "placeReviewWrite";
+	}
+	
+	@GetMapping("placeReviewDetail.pla")
+	public String placeReviewDetail() {
+		return "placeReviewDetail";
+	}
+}
